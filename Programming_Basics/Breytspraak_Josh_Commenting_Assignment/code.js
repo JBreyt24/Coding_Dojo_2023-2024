7372c6aa09b2b187ae10e07dd5f4095580b150cc
Josh Breytspraak <jbreyt24@gmail.com>
2023-04-15 21:00:00 -0500

    commenting assignment

---
+++ Programming_Basics/Breytspraak_Josh_Commenting_Assignment/code.js
@@ -0,0 +1,7 @@
+//replace x for likeCount to represent likes
+var likeCount = 0;
+//this will print out how many likes there are
+function increaselikes(){
+    //this will take the original like count and add one to every loop
+    likeCount = likeCount + 1;
+}
