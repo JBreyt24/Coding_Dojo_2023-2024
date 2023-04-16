e3281d8ae101f5e47ec374a04af0970cd227b1be
Josh Breytspraak <jbreyt24@gmail.com>
2023-04-15 21:00:00 -0500

    javascript assignments for programming basics class

---
+++ Programming_Basics/JavaScript/Breytspraak_Josh_Challenge_Operators/Breytspraak_Josh_Operators.js
@@ -0,0 +1,93 @@
+//As a user, I need to know how many pieces of cake I will have left if I divide out my cake (which has 12 pieces) for 5 people to eat.
+//As the designated cake slicer and distributor at our office birthday parties, 
+//I want a function called howMuchLeftOverCake that passes two variables numberOfPieces and numberOfPeople into it and figures out the remaining pieces of cake.
+//
+
+
+// var numberOfPieces = 12
+// var numberOfPeople = 5
+// var leftOverPieces = numberOfPieces % numberOfPeople
+
+// function howMuchLeftOverCake() {
+//     for (var numberOfPieces = 12; numberOfPieces >= 0; numberOfPieces--) {
+//         if (leftOverPieces >= 5) {
+//             return leftOverPieces
+//         }
+//         console.log("Hold another party!")
+
+//         if(leftOverPieces = 3 && 5 ) {
+//             return leftOverPieces
+//         }
+//             console.log("You have leftovers to share!")
+
+//         if (leftOverPieces = 1 && 2) {
+//             return leftOverPieces
+//         }
+//             console.log("You have some leftovers!")
+
+//         if (leftOverPieces = 0) {
+//             return leftOverPieces
+//         }
+//         console.log("No leftovers for you!")
+// }
+// }
+// howMuchLeftOverCake(12,5)
+
+
+
+
+// Complete the function in the code below to console log all numbers down from 68 to 9.
+
+// var i = 68
+// function print68to9(){
+//     for (var i=68; i>= 9; i--){
+//             if (i < 68);
+//             console.log(i)
+//     }
+// }
+// print68to9();
+
+
+
+
+
+
+
+
+
+
+// Question 9
+// Complete the function in the code below to return the largest value of an array.
+// Given: [3, 6, 4, 9, 2]
+// Return: 9
+
+var arr = 
+
+function findLargest(arr) {
+    // your code here
+}
+
+findLargest([12, 21, 3.6, 9, 12, 8]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var arr = [1, 4, 7, 6, 2, 1];
+// var count = 0;
+// for(var i=0; i<arr.length; i++) {
+//     if(arr[i] > 3) {
+//         count++;
+//     }
+// }
+// console.log(count);
---
+++ Programming_Basics/JavaScript/Breytspraak_Josh_JSPredict/Breytspraak_Josh_JSPredict.js
@@ -0,0 +1,24 @@
+//Predict 1. "I was born in 1980"
+
+//Predict 2. "I was born in 1980"
+
+//Predict 3. "Summing Number!"
+//           "num1 is: 10"
+//           "num1 is: 20"
+//           "30"
+
+
+
+// function hello(){
+//     console.log("hello");
+//     return 15;
+// }
+// var result = hello();
+// console.log("result is", result);
+
+function numPlus(num) {
+    console.log("num is" , num);
+    return num+15;
+}
+var result = numPlus(3);
+console.log("result is" , result);
